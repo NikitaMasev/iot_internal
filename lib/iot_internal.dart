c5214eddc7067cc22efd8065b93e9ff5c2a209d2
Nikita Masev <n.masev@bastion-tech.ru>
2023-11-23 12:51:10 +0300

    removed repeated interfaces; integrated launchable_interfaces;

--- lib/iot_internal.dart
+++ lib/iot_internal.dart
@@ -4,8 +4,6 @@ export 'base/app_bloc_observer.dart';
 export 'base/custom_codec.dart';
 export 'base/pausable.dart';
 export 'base/resumable.dart';
-export 'base/runnable.dart';
-export 'base/runnable_async.dart';
 export 'codecs/client_codec.dart';
 export 'codecs/client_codec_impl.dart';
 export 'codecs/iot_devices_codec.dart';
